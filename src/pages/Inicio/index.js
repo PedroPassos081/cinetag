9299508c20c280657c5431067a1d33e2cfd5ab53
PedroPassos081 <pedro.passos081@gmail.com>
2024-01-12 19:40:59 -0300

    Criando as "cards" do projeto

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -2,6 +2,7 @@ import Cabecalho from "components/Cabecalho";
 import Banner from "components/Banner";
 import Rodape from "components/Rodape";
 import Titulo from "components/Titulo";
+import Card from "components/Card";
 
 function Inicio() {
     return (
@@ -11,6 +12,7 @@ function Inicio() {
             <Titulo>
                 <h1>UM lugar para guardar seus vídeos e filmes!</h1>
             </Titulo>
+            <Card id='1' titulo='Gato de botas' capa='https://www.google.com/url?sa=i&url=https%3A%2F%2Foregional.com.br%2Fblog%2Fdetalhes%2Fgato-de-botas-2-o-ultimo-pedido-aposta-nos-classicos-para-resgatar-a-dreamworks&psig=AOvVaw1Jio5XQyVllNZjfRc9CoYY&ust=1705185274457000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCICruaj02IMDFQAAAAAdAAAAABAD' />
              <Rodape />
         </>
     )
