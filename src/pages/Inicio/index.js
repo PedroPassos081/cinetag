79d1b72151d265dd2aef7f5f6fd421f7e3168e5f
PedroPassos081 <pedro.passos081@gmail.com>
2024-01-12 19:19:18 -0300

    Adicioando titulo do projeto

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,12 +1,16 @@
 import Cabecalho from "components/Cabecalho";
 import Banner from "components/Banner";
 import Rodape from "components/Rodape";
+import Titulo from "components/Titulo";
 
 function Inicio() {
     return (
         <>
             <Cabecalho />
             <Banner imagem="home" />
+            <Titulo>
+                <h1>UM lugar para guardar seus vídeos e filmes!</h1>
+            </Titulo>
              <Rodape />
         </>
     )
