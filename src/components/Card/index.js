677a5e8306c47a2bf3966bfd05a9db561f24b657
Pedro Passos <pedro.passos081@gmail.com>
2024-01-23 20:31:03 -0300

    Adicionando a função de favoritas os filmes

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,15 +1,22 @@
+import { useFavoritoContext } from 'contextos/Favoritos'
 import styles from './Card.module.css'
 import iconeFavoritar from './favoritar.png'
-
+import iconeDesfavoritar from './desfavoritar.png'
 
 function Card({id, titulo, capa}) {
+    const { favorito, adicionarFavorito } = useFavoritoContext()
+    const EhFavorito = favorito.some((fave) => fave.id ===id)
+    const icone = !EhFavorito ? iconeFavoritar : iconeDesfavoritar;
     return (
         <div className={styles.container}>
             <img src={capa} alt="Capa do jogo" className={styles.capa}/>
             <h2>{titulo}</h2>
-            <img src={iconeFavoritar}
+            <img src={icone}
             alt='Favoritar filme'
-            className={styles.favoritar} />
+            className={styles.favoritar} 
+            onClick={() => {
+                adicionarFavorito({ id, titulo, capa })
+            }}/>
         </div>
     )
 }
