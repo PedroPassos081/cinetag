af52068cfcbca56e506a47e4f1b8edc971441af9
Pedro Passos <pedro.passos081@gmail.com>
2024-01-26 19:18:06 -0300

    Finalizando projeto

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,22 +1,31 @@
 import Banner from "components/Banner";
-import Titulo from "components/Titulo";
 import Card from "components/Card";
-import videos from 'json/db.json';
-import styles from './Inicio.module.css'
+import Titulo from "components/Titulo";
+import { useEffect, useState } from "react";
+import styles from './Inicio.module.css';
 
 function Inicio() {
+    const [videos, setVideos] = useState([]);
+
+    useEffect(() => {
+        fetch('https://my-json-server.typicode.com/monicahillman/cinetag-api/videos')
+            .then(resposta => resposta.json())
+            .then(dados => {
+                setVideos(dados)
+            })
+    }, [])
+
     return (
         <>
             <Banner imagem="home" />
             <Titulo>
-                <h1>UM lugar para guardar seus vídeos e filmes!</h1>
+                <h1>Um lugar para guardar seus vídeos e filmes!</h1>
             </Titulo>
             <section className={styles.container}>
                 {videos.map((video) => {
-                   return <Card {...video} key={video.id} />
+                    return <Card {...video} key={video.id} />
                 })}
             </section>
-             
         </>
     )
 }
--- src/pages/Player/index.js
+++ src/pages/Player/index.js
@@ -1,40 +1,43 @@
-import Banner from "components/Banner";
+import Banner from 'components/Banner';
 import Titulo from 'components/Titulo';
+import { useParams } from 'react-router-dom';
 import styles from './Player.module.css';
-import videos from 'json/db.json';
-import { useParams } from "react-router-dom";
-import NaoEncontrada from "pages/NaoEncontrada";
+import NaoEncontrada from 'pages/NaoEncontrada';
+import { useEffect, useState } from 'react';
 
 function Player() {
+    const [video, setVideo] = useState();
     const parametros = useParams();
-    const video = videos.find((video => {
-        return video.id === Number(parametros.id);
-    }))
+
+    useEffect(() => {
+        fetch(`https://my-json-server.typicode.com/monicahillman/cinetag-api/videos?id=${parametros.id}`)
+            .then(resposta => resposta.json())
+            .then(dados => {
+                setVideo(...dados)
+            })
+    }, [])
 
     if (!video) {
         return <NaoEncontrada />
     }
-    
 
-return (
-    <>
-        <Banner imagem="player" />
-        <Titulo>
-            <h1>Player</h1>
-        </Titulo>
-        <section className={styles.container}>
-        <iframe
-            width="100%"
-            height="100%"
-            src={video.link}
-            title={video.titulo}
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
-
-        </iframe>
-        </section>
-    </>
-)
+    return (
+        <>
+            <Banner imagem="player" />
+            <Titulo>
+                <h1>Player</h1>
+            </Titulo>
+            <section className={styles.container}>
+                <iframe
+                    width="100%"
+                    height="100%"
+                    src={video.link}
+                    title={video.titulo}
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </section>
+        </>
+    )
 }
 
 export default Player;
